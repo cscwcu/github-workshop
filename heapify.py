e2de5000f2d6ad6e0b8e919e4e046640ebe073ac
Daniel <76850126+fullstack-dan@users.noreply.github.com>
2024-09-12 15:51:22 -0400

    reverting files

--- heapify.py
+++ heapify.py
@@ -7,42 +7,34 @@ def heapify(arr, n, i):
     left = 2 * i + 1
     right = 2 * i + 2
 
-    if left < n and arr[left] < arr[smallest]:
+    if left < n and arr[left] > arr[smallest]:
         smallest = left
-    if right < n and arr[right] < arr[smallest]:
+    if right < n and arr[right] > arr[smallest]:
         smallest = right
 
     if smallest != i:
         arr[i], arr[smallest] = arr[smallest], arr[i]
         heapify(arr, n, smallest)
 
-
-def build_min_heap(arr):
+def buildMinHeap(arr):
     n = len(arr)
     startIdx = n // 2 - 1
     for i in range(startIdx, -1, -1):
         heapify(arr, n, i)
 
-
 def heapify_arrays_from_file(file_path):
     with open(file_path, 'r') as file:
         for line in file:
-            # Convert the line to a list of integers
-            arr = ast.literal_eval(line.strip())
+            arr = line.strip()
 
-            build_min_heap(arr)
+            buildMinHeap(arr)
 
             print("Min Heap:", arr)
 
 
 def main():
-    # Get the first command-line argument as the input file path
-    if len(sys.argv) > 1:
-        file_path = sys.argv[1]
-        heapify_arrays_from_file(file_path)
-    else:
-        print("Please provide a file path as an argument.")
-
+    file = sys.argv[0]
+    heapify_arrays_from_file(file)
 
 if __name__ == "__main__":
     main()
