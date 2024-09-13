df40d7f809ba9aecfc8ca253fbc9bce0658e9d7c
Sample-Program <shp.plemmons@gmail.com>
2024-09-13 18:32:58 -0400

    finished code

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,9 +4,9 @@ import ast
 def buble_sort(arr):
     n = len(arr)
     for i in range(n):
-        for j in range(0, n-i):
+        for j in range(0, n-i-1):
             # Swap if the element found is greater than the next element
-            if arr[j] > arr[j+1]:
+            if arr[j] > arr[j-1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
