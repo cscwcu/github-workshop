c28bc1f4a6ed9d5daecf06158f1098b05a72d9ce
Baptiste Perdrix <bperdrix1@catamount.wcu.edu>
2024-09-13 18:37:59 -0400

    fixed

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,7 +4,7 @@ import ast
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        for j in range(0, n-1):
+        for j in range(0, n-i-1):
             # Swap if the element found is greater than the next element
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
